4997bd4e4a5d7fd3a92d107654a3c1126dd78f57
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-04-03 11:31:09 +0300

    add search and filtr for tasks in admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -5,6 +5,8 @@ from todo.models import Tag, Task
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ["content", "created_datetime", "deadline_datetime", "is_done"]
+    list_filter = ["tags"]
+    search_fields = ["content"]
 
 
 admin.site.register(Tag)
