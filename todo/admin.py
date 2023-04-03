55ed1b733bf52b5e1c70e5a5d99f9a52eb8b118f
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-04-03 12:18:34 +0300

    add base start page

--- todo/admin.py
+++ todo/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from todo.models import Tag, Task
 
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ["content", "created_datetime", "deadline_datetime", "is_done"]
     list_filter = ["tags"]
@@ -10,4 +11,3 @@ class TaskAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Tag)
-admin.site.register(Task, TaskAdmin)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from todo.models import Tag, Task
+
+
+def index(request):
+    tasks = Task.objects.all()
+    context = {
+        "tasks": tasks,
+    }
+    return render(request, "todo/index.html", context=context)
+
+
+class CarListView(generic.ListView):
+    model = Tag
