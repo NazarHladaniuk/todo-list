d4e84aaa8526874f722bd4432a9d65b00f1a09aa
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-04-03 11:28:47 +0300

    modify tasks admin panel

--- todo/admin.py
+++ todo/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from todo.models import Tag, Task
 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "created_datetime", "deadline_datetime", "is_done"]
+
+
 admin.site.register(Tag)
-admin.site.register(Task)
+admin.site.register(Task, TaskAdmin)
