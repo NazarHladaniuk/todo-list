7e2dfe5acd73a345f288fda507b08ee345984092
Nazar Hladaniuk <powerlolek46@gmail.com>
2023-04-04 13:45:01 +0300

    add and implement crispy forms

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Tag, Task
 
 
@@ -35,13 +36,13 @@ class TaskListView(generic.ListView):
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("todo:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
     success_url = reverse_lazy("todo:task-list")
 
 
